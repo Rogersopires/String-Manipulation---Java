ebc55029ad77e4cf2084ba3d388601f66ba1bad7
RogersPires <rogers.o.pires@gmail.com>
2023-12-04 20:58:25 -0300

    Resolução do problema 001

--- stringmanipulation/src/main/java/bng/rogers/stringmanipulation/StringmanipulationApplication.java
+++ stringmanipulation/src/main/java/bng/rogers/stringmanipulation/StringmanipulationApplication.java
@@ -1,13 +1,13 @@
 package bng.rogers.stringmanipulation;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StringmanipulationApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(StringmanipulationApplication.class, args);
-	}
+		//SpringApplication.run(StringmanipulationApplication.class, args);
+		Resolution001 resposta001 = new Resolution001();
+		resposta001.main();
+		}
 
 }
